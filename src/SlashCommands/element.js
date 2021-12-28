98dc9ea2d4f40a4ae817e108b8f590e49868b354
LinkWasTaken <zechariahpeoples@gmail.com>
2021-12-28 02:23:02 -0600

    Update element.js
    Finally updated/created elemental reaction searching. Might add element specific searches?

--- src/SlashCommands/element.js
+++ src/SlashCommands/element.js
@@ -1,16 +1,15 @@
-/* eslint-disable consistent-return */
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const emote = require('../../assets/emotes.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('element')
-		.setDescription('Retrieve information on specific elements.')
+		.setDescription('Retrieve information on specific elements/reactions.')
 		.addStringOption(option =>
 			option
 				.setName('vision')
-				.setDescription('The character\'s vision.')
+				.setDescription('The desired vision.')
 				.addChoices([
 					['Pyro', 'Pyro'],
 					['Dendro', 'Dendro'],
@@ -21,21 +20,45 @@ module.exports = {
 					['Anemo', 'Anemo']
 				])),
 	async run({ paimonClient, application }) {
-		const visionType = application.options.getString('vision');
-		const elementList = paimonClient.elements;
-		const reactions = this.formatReactions(elementList);
-		console.log(reactions);
+		const elementList = paimonClient.elements.map(ele => ele);
+
+		let reactionsArray = [];
+
+		for (let i = 0; i < elementList.length; i++) {
+			for (let j = 0; j < elementList[i].reactions.length; j++) {
+				const reactionObj = {
+					name: elementList[i].reactions[j].name,
+					reactionFormula: elementList[i].reactions[j].elements,
+					initialElement: elementList[i].name,
+					description: elementList[i].reactions[j].description
+				};
+
+				reactionsArray.push(reactionObj);
+			}
+		}
+
+		reactionsArray = reactionsArray.filter((value, index, self) =>
+			index === self.findIndex((item) =>
+				item.place === value.place && item.name === value.name
+			));
+
+		const elementalReactionEmbed = new MessageEmbed()
+			.setTitle('Elemental Reactions')
+			.setThumbnail('https://i.ibb.co/HD4gKwc/Icon-Elemental-Sight.png')
+			.setColor('WHITE');
+
+		for (let i = 0; i < reactionsArray.length; i++) {
+			const { name, initialElement, description, reactionFormula } = reactionsArray[i];
+			elementalReactionEmbed.addField(`${name}\n${this.convertReactionsToEmojis(reactionFormula, initialElement)}`, `${description}`, true);
+		}
+
+		return application.followUp({ embeds: [elementalReactionEmbed] });
 	},
 
-	formatReactions(elementList) {
-		const formattedList = elementList.map(ele => {
-			for (let i = 0; i < ele.reactions.length; i++) {
-				ele.reactions[i].element.push(ele.name);
-			}
-
-			return ele;
-		});
-
-        console.log(formattedList);
+	convertReactionsToEmojis(reactionFormulaArray, initialElement) {
+		const initialElementEmoji = emote[initialElement.toLowerCase()];
+		const reactionFormulaEmoji = reactionFormulaArray.map(ele => emote[ele.toLowerCase()]);
+		reactionFormulaEmoji.push(initialElementEmoji);
+		return reactionFormulaEmoji.join(' ');
 	}
 };
