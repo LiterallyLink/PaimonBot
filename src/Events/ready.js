6cc0c00533d47a9f1817d99aa29e5c0c256504b6
LinkWasTaken <zechariahpeoples@gmail.com>
2021-12-28 02:20:40 -0600

    Update ready.js
    Extra logging for items

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -12,7 +12,8 @@ module.exports = class extends Event {
 		console.log([
 			`Logged in as ${this.client.user.tag}`,
 			`Cached ${this.client.elements.size} elements`,
-			`Cached ${this.client.characters.size} characters`
+			`Cached ${this.client.characters.size} characters`,
+			`Cached ${this.client.weapons.size} weapons`
 		].join('\n'));
 
 
