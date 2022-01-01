0718f625c5187a6fa3f176e72367f0576036cd92
LinkWasTaken <zechariahpeoples@gmail.com>
2022-01-01 00:58:48 -0600

    Create Mongo.js

---
+++ src/Structures/Mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { mongoPath } = require('../../config.json');
+
+module.exports = {
+	init: () => {
+		const dbOptions = {
+			autoIndex: false,
+			maxPoolSize: 5,
+			connectTimeoutMS: 10000,
+			family: 4
+		};
+
+		mongoose.connect(mongoPath, dbOptions);
+		mongoose.Promise = global.Promise;
+
+		mongoose.connection.on('connected', () => {
+			console.log('Successfully connected to the database.');
+		});
+
+		mongoose.connection.on('err', err => {
+			console.error(`Mongoose connection error: \n${err.stack}`);
+		});
+
+		mongoose.connection.on('disconnected', () => {
+			console.warn('Database connection lost.');
+		});
+	}
+};
