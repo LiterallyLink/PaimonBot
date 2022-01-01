16d86b37aa9cb2244f8902e5a4ed0ceafb124a86
LinkWasTaken <zechariahpeoples@gmail.com>
2022-01-01 00:58:02 -0600

    Update ready.js
    Removed element logging

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -11,9 +11,10 @@ module.exports = class extends Event {
 	async run() {
 		console.log([
 			`Logged in as ${this.client.user.tag}`,
-			`Cached ${this.client.elements.size} elements`,
 			`Cached ${this.client.characters.size} characters`,
-			`Cached ${this.client.weapons.size} weapons`
+			`Cached ${this.client.weapons.size} weapons`,
+			`Cached ${this.client.artifacts.size} artifact sets`,
+			`Cached ${this.client.artifacts.map(art => art).flat().length} artifacts`
 		].join('\n'));
 
 
