026b184e7e5e744aaf1e9f1286c0cc210ccc3539
LinkWasTaken <zechariahpeoples@gmail.com>
2022-01-13 01:40:48 -0600

    Using local image instead

--- src/SlashCommands/ping.js
+++ src/SlashCommands/ping.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const { paimonIsAnAlcholic } = require('../../assets/paimonImages.json');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,11 +10,12 @@ module.exports = {
 
 		const apiLatency = `\`${Math.round(paimonClient.ws.ping)}ms\``;
 		const uptime = `\`${paimonClient.utils.msToTime(paimonClient.uptime, { long: true })}\``;
+		const attachment = new MessageAttachment('.\\assets\\images\\paimon\\alchoholicPaimon.webp', 'alchoholicPaimon.webp');
 
 		const pingResponseEmbed = new MessageEmbed()
-			.setThumbnail(paimonIsAnAlcholic)
+			.setThumbnail(`attachment://${attachment.name}`)
 			.setDescription(`Bot Latency: \`${msg.createdTimestamp - application.createdTimestamp}\`ms\nWS Latency: ${apiLatency}\n Uptime: ${uptime}`)
 			.setColor('WHITE');
-		return application.editReply({ content: '_ _', embeds: [pingResponseEmbed] });
+		return application.editReply({ content: '_ _', embeds: [pingResponseEmbed], files: [attachment] });
 	}
 };
