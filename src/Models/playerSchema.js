497f178ed21f31ad1302d2f1f0ab0da42dc01db9
LinkWasTaken <zechariahpeoples@gmail.com>
2022-01-13 20:56:39 -0600

    Create playerSchema.js

---
+++ src/Models/playerSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+	userId: { type: String, required: true },
+	mora: { type: Number, default: 0 },
+	primogems: { type: Number, default: 0 },
+	fishingRod: { type: String, default: 'Wilderness Rod' },
+	adventureRank: { type: Number, default: 1 },
+	adventureRankEXP: { type: Number, default: 0 },
+	worldLevel: { type: Number, default: 0 },
+	namecards: { type: Array, default: [] },
+	currentCard: { type: String, default: 'default' },
+	createdOn: { type: Date, default: new Date().getTime() },
+	lastUpdated: { type: Date, default: new Date().getTime() },
+	isBlacklisted: { type: Boolean, default: false },
+	dailyLastClaimedAt: { type: Date, default: 0 },
+	dailyClaimedStreak: { type: Number, default: 0 },
+	increasedFiveStarChance: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('profile', playerSchema, 'profiles');
