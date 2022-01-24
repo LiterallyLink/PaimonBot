d896d0a8aeaa12f262f748af2070d177df655740
LinkWasTaken <zechariahpeoples@gmail.com>
2022-01-24 02:24:08 -0600

    Update playerSchema.js
    Minor changes to playerData

--- src/Models/playerSchema.js
+++ src/Models/playerSchema.js
@@ -3,18 +3,16 @@ const mongoose = require('mongoose');
 const playerSchema = new mongoose.Schema({
 	userId: { type: String, required: true },
 	mora: { type: Number, default: 0 },
-	fishingRod: { type: String, default: 'Wilderness Rod' },
-	adventureRank: { type: Number, default: 1 },
-	adventureRankEXP: { type: Number, default: 0 },
-	worldLevel: { type: Number, default: 0 },
+	inventory: { type: Array, default: [] },
 	namecards: { type: Array, default: [] },
-	currentCard: { type: String, default: 'default' },
+	currentCard: { type: String, default: 'Default' },
+	totalWishes: { type: Number, default: 0 },
+	fourStarPity: { type: Number, default: 0 },
+	fiveStarPity: { type: Number, default: 0 },
 	createdOn: { type: Date, default: new Date().getTime() },
 	lastUpdated: { type: Date, default: new Date().getTime() },
-	isBlacklisted: { type: Boolean, default: false },
 	dailyLastClaimedAt: { type: Date, default: 0 },
-	dailyClaimedStreak: { type: Number, default: 0 },
-	increasedFiveStarChance: { type: Number, default: 0 }
+	dailyClaimedStreak: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('profile', playerSchema, 'profiles');
