9aa41770692a435377d9f642b7733d6c4548385a
celestialgaze <46394932+celestialgaze3@users.noreply.github.com>
2022-02-03 21:28:27 -0600

    fix bot

--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
-const PaimonClient = require('./Structures/PaimonClient');
-const config = require('../config.json');
-
-const client = new PaimonClient(config);
-
-process.on('unhandledRejection', (reason, pr) => {
-	console.log(' [antiCrash] :: Unhandled Rejection/Catch');
-	console.log(reason, pr);
-});
-
-process.on('uncaughtException', (err, origin) => {
-	console.log(' [antiCrash] :: Uncaught Exception/Catch');
-	console.log(err, origin);
-});
-
-process.on('uncaughtExceptionMonitor', (err, origin) => {
-	console.log(' [antiCrash] :: Uncaught Exception/Catch (MONITOR)');
-	console.log(err, origin);
-});
-
-process.on('multipleResolves', () => {
-	console.log(' [antiCrash] :: Multiple Resolves');
-});
-
-client.start();
+const penis = "penis";
+console.log(penis);
