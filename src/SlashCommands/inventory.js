eccd73167f67b02477521acb8d279305eb6c1dda
LinkWasTaken <zechariahpeoples@gmail.com>
2022-02-07 16:30:26 -0600

    Update inventory.js
    minor changes and modifications

--- src/SlashCommands/inventory.js
+++ src/SlashCommands/inventory.js
@@ -2,7 +2,6 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
 
 module.exports = {
-
 	data: new SlashCommandBuilder()
 		.setName('inventory')
 		.setDescription("View a list of weapons and characters you've pulled"),
@@ -11,20 +10,34 @@ module.exports = {
 		const characterList = paimonClient.characters;
 		const weaponList = paimonClient.weapons;
 
-		const characters = [];
-		const weapons = [];
+		const structuredInventory = inventory.reduce((acc, cur) => {
+			const existing = acc.find(i => i.name === cur);
 
-		for (let i = 0; i < inventory.length; i++) {
-			const { name, count } = inventory[i];
+			if (existing) existing.count++;
+			else acc.push({ name: cur, count: 1 });
+
+			return acc;
+		}, []);
+
+		const characterArr = [];
+		const weaponArr = [];
+
+		for (let i = 0; i < structuredInventory.length; i++) {
+			const { name, count } = structuredInventory[i];
 
 			if (characterList.has(name)) {
 				const { rarity } = characterList.get(name);
-				characters.push({ name, rarity, count });
+				characterArr.push({ name, rarity, count });
 			} else if (weaponList.has(name)) {
 				const { rarity } = weaponList.get(name);
-				weapons.push({ name, rarity, count });
+				weaponArr.push({ name, rarity, count });
 			}
 		}
+
+		const totalCharacters = weaponArr.reduce((acc, cur) => acc + cur.count, 0);
+		const totalWeapons = weaponArr.reduce((acc, cur) => acc + cur.count, 0);
+		characterArr.sort((a, b) => b.rarity - a.rarity);
+		weaponArr.sort((a, b) => b.rarity - a.rarity);
 
 		const optionRow = new MessageActionRow()
 			.addComponents(
@@ -46,70 +59,41 @@ module.exports = {
 					.setStyle('PRIMARY')
 			);
 
-		let totalCharacters = 0;
-		let totalWeapons = 0;
-
-		for (let i = 0; i < characters.length; i++) {
-			totalCharacters += characters[i].count;
-		}
-
-		for (let i = 0; i < weapons.length; i++) {
-			totalWeapons += weapons[i].count;
-		}
-
 		const inventoryEmbed = new MessageEmbed()
-			.setTitle('Inventory')
+			.setAuthor({ name: `${application.user.username}'s Inventory`, iconURL: application.user.avatarURL() })
 			.addField('Total Wishes', `${totalWishes}`, true)
-			.addField('Characters', `${characters.length ? totalCharacters : 'None'}`, true)
-			.addField('Weapons', `${weapons.length ? totalWeapons : 'None'}`, true)
+			.addField('Total Characters', `${characterArr.length ? totalCharacters : 'None'}`, true)
+			.addField('Total Weapons', `${weaponArr.length ? totalWeapons : 'None'}`, true)
 			.setColor('WHITE');
 		const msg = await application.followUp({ embeds: [inventoryEmbed], components: [optionRow] });
 
 		const filter = i => i.user.id === application.user.id;
-
 		const collector = msg.createMessageComponentCollector({ filter, time: 300000 });
 
 		return collector.on('collect', async i => {
 			i.deferUpdate();
 			const { customId } = i;
-
 			if (customId === 'delete') {
 				collector.stop();
 				msg.delete().catch(() => null);
 			}
-
 			if (customId === 'characters') {
+				optionRow.components[0].setDisabled(true);
+				optionRow.components[1].setDisabled(false);
 				const characterEmbed = new MessageEmbed()
-					.setTitle('Characters')
+					.setAuthor({ name: `${application.user.username}'s Characters`, iconURL: application.user.avatarURL() })
+					.setDescription(`${characterArr.length ? characterArr.map(char => `${char.count}x ${char.name}`).join('\n') : "You haven't summoned any characters yet!"}`)
 					.setColor('WHITE');
-
-				for (let j = 0; j < 2; j++) {
-					const rarity = j + 4;
-
-					const filteredCharList = characters.filter(char => char.rarity === rarity).sort((a, b) => b.count - a.count);
-					if (!filteredCharList.length) continue;
-
-					characterEmbed.addField(`${rarity} ⭐`, `${filteredCharList.map(char => `${char.count}x ${char.name}`).join('\n')}`, true);
-				}
-
-				application.editReply({ embeds: [characterEmbed], components: [optionRow] });
+				msg.edit({ embeds: [characterEmbed], components: [optionRow] });
 			}
-
 			if (customId === 'weapons') {
-				const weaponEmbed = new MessageEmbed()
-					.setTitle('Weapons')
+				optionRow.components[0].setDisabled(false);
+				optionRow.components[1].setDisabled(true);
+				const characterEmbed = new MessageEmbed()
+					.setAuthor({ name: `${application.user.username}'s Weapon's`, iconURL: application.user.avatarURL() })
+					.setDescription(`${weaponArr.length ? weaponArr.map(char => `${char.count}x ${char.name}`).join('\n') : "You haven't pulled any weapons yet!"}`)
 					.setColor('WHITE');
-
-				for (let j = 0; j < 3; j++) {
-					const rarity = j + 2;
-
-					const filteredWeaponList = weapons.filter(weap => weap.rarity === rarity).sort((a, b) => b.count - a.count);
-					if (!filteredWeaponList.length) continue;
-
-					weaponEmbed.addField(`${rarity} ⭐`, `${filteredWeaponList.map(weap => `${weap.count}x ${weap.name}`).join('\n')}`, true);
-				}
-
-				application.editReply({ embeds: [weaponEmbed], components: [optionRow] });
+				msg.edit({ embeds: [characterEmbed], components: [optionRow] });
 			}
 		});
 	}
